beafa807af978da0efec5ebd36077661bf6d4bca
Wafflexan <101963408+Wafflexan@users.noreply.github.com>
2026-02-16 21:55:14 -0800

    Some Work

--- Scripts/death_scene.gd
+++ Scripts/death_scene.gd
@@ -1,16 +1,23 @@
 extends CanvasModulate
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var button: Button = $Control/Button
+
+var pressed : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pressed = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	await animation_player.animation_finished
+	SceneManager._change_scene("MAIN")
 
 func _on_button_pressed() -> void:
-	animation_player.play("light beam")
+	if pressed == false:
+		pressed = true
+		button.visible = false
+		animation_player.play("light beam")
 	pass # Replace with function body.
