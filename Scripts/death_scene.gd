c088e5164480117d58df96d140ee54d4824bd947
Owen-Harper25 <pistongames25@gmail.com>
2026-02-17 17:24:37 -0800

    Improved start animation

--- Scripts/death_scene.gd
+++ Scripts/death_scene.gd
@@ -1,18 +1,22 @@
 extends CanvasModulate
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var button: Button = $Control/Button
+@onready var bus_stop: Sprite2D = $"Bus stop"
+@onready var animation_player_3: AnimationPlayer = $AnimationPlayer3
 
 var pressed : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pressed = false
-	pass # Replace with function body.
-
+	animation_player.play("Bus stop")
+	animation_player_3.play("Death")
+	await animation_player.animation_finished
+	animation_player.play("light beam")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	await animation_player.animation_finished
+	await animation_player_3.animation_finished
 	SceneManager._change_scene("MAIN")
 
 func _on_button_pressed() -> void:
